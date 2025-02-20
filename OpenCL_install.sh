79e45ab577fcfaf0546850cf069552265e298828
semvlu <74308718+semvlu@users.noreply.github.com>
2025-02-20 15:40:57 +0800

    Delete OpenCL_install.sh

--- OpenCL_install.sh
+++
@@ -1,8 +0,0 @@
-# OpenCL install
-
-git clone https://github.com/microsoft/vcpkg.git
-cd vcpkg
-bootstrap-vcpkg.bat
-
-vcpkg install <lib> # in casu opencl
-vcpkg integrate install # integrate w/ VS 2022
